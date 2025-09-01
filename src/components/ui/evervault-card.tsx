c2f7b52902acc30169432a28fbc2f4eb15a83d8a
Aditya Banerjee <86280593+Aditya12705@users.noreply.github.com>
2025-09-01 13:25:29 +0530

    Update evervault-card.tsx

--- src/components/ui/evervault-card.tsx
+++ src/components/ui/evervault-card.tsx
@@ -51,7 +51,7 @@ export const EvervaultCard = ({
           randomString={randomString}
         />
         <div className="relative z-10 flex items-center justify-center">
-          <div className="relative h-44 w-44 rounded-full flex items-center justify-center text-white font-bold text-4xl">
+          <div className="relative h-32 w-32 md:h-44 md:w-44 rounded-full flex items-center justify-center text-white font-bold text-2xl md:text-4xl">
             <div className="absolute w-full h-full rounded-full" style={{ backgroundColor: "#C2A66C" }} />
             <span className="dark:text-white text-black z-20">{text}</span>
           </div>
